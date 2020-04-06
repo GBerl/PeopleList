2e6ecbd205a10a97096449eb1e72786b4cdd5da0
Gitty Berl <gittyberl@gmail.com>
2020-04-05 21:13:26 -0700

    Add project files.

---
+++ People.Data/Peopledb.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace People.Data
+{
+    public class Peopledb
+    {
+        private string _connection;
+        public Peopledb(string connection)
+        {
+            _connection = connection;
+        }
+        public List<Person> ShowAllPeople()
+        {
+            using (SqlConnection conn = new SqlConnection(_connection))
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "Select * from People";
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Person> people = new List<Person>();
+                while (reader.Read())
+                {
+                    people.Add(new Person
+                    {
+                        FirstName = (string)reader["FirstName"],
+                        LastName = (string)reader["LastName"],
+                        Age = (int)reader["Age"],
+                        Id = (int)reader["Id"]
+
+                    });
+                }
+                return people;
+            }
+
+
+        }
+    }
+}
